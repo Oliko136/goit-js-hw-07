8ad79e1d01a67189b03e6e57efb5944723b3f1ab
Oliko136 <olchick136@gmail.com>
2022-08-28 16:06:08 +0400

    Модальне вікно з бібліотеки підключено

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,19 +4,38 @@ const gallery = document.querySelector('.gallery');
 
 renderGallery(galleryItems);
 
+gallery.addEventListener('click', onModalOpen);
+
 function renderGallery(items) {
     const markup = items.map(({preview, original, description}) =>
-        `<a class="gallery__link" href="${original}">
-         <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-            />
-         </a>`
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+         </div>`
     ).join('');
 
     gallery.insertAdjacentHTML('beforeend', markup);
 }
 
-console.log(galleryItems);
+function onModalOpen(event) {
+    event.preventDefault();
+    
+    const imageEl = event.target.classList.contains('gallery__image');
+    const bigImageURL = event.target.dataset.source;
+
+    if (!imageEl) {
+        return;
+    };
+
+    const instance = basicLightbox.create(`
+    <img src="${bigImageURL}">
+    `);
+
+    instance.show();
+}
